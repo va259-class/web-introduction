53f07e96b81667a865c4b5a0a0759f11495663ba
va259-class <va259@outlook.com>
2026-01-31 10:33:06 +0300

    First Step

---
+++ Vektorel.HelloWeb/HelloWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HelloWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
